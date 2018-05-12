06f6590e4872b0720916b6d1544584371267b8ac
chengdai <chengzhendai@gmail.com>
2018-05-11 22:40:55 -0400

    initial pipeline commit

---
+++ src/identify_protein_variants.py
@@ -0,0 +1,43 @@
+import glob
+import os
+import subprocess
+import argparse
+
+'''NOTE: When in doubt, call "python [FILENAME.py] -h" to get a description of the necessary parameters'''
+
+# Get arguments
+parser = argparse.ArgumentParser(description='Specify arguments for usearch local alignment of ARGs.')
+parser.add_argument('--fasta_folder',
+                    help='path to folder containing quality controlled fasta files')
+parser.add_argument('--db',
+                    help='path to basename of the db for the reference markers')
+parser.add_argument('--out',
+                    help='folder for all usearch output files')
+parser.add_argument('--usearch', default='usearch',
+                    help='path to usearch software')
+parser.add_argument('--threads', default= 1.0,
+                    help='number threads to use for alignment')
+args = parser.parse_args()
+
+#Get all fasta file in folder (NOTE: fasta files should be QC'ed)
+fasta_folder = args.fasta_folder
+files = glob.glob(fasta_folder + '*.fasta*')
+out_path = args.out
+
+#usearch parameters
+db = args.indexed_db
+usearch = args.usearch + '  -usearch_local'
+threads = args.threads
+
+for f in files:
+    print 'Finding variants for file: {0}'.format(f.split('/')[-1])
+
+    report_out_file = out_path + f.split('/')[-1].split('.')[0] + '_protein_snp.txt'
+    
+    command = "{0} {1} -db {2} -target_cov 1.0 -id 1.0 -maxhits 1 -threads {3} -userout {4} -userfields query+target+id+evalue+qcov+tcov+ql+tl+bits".format(usearch, f, db, threads, report_out_file)
+
+    final = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+    (stdout, stderr) = final.communicate()
+    print stdout
+    print stderr
+
---
+++ src/quantify_resistance.py
@@ -0,0 +1,61 @@
+import glob
+import multiprocessing
+import subprocess
+import argparse
+import random
+
+'''NOTE: When in doubt, call "python [FILENAME.py] -h" to get a description of the necessary parameters'''
+
+def quantify_resistomes(f):
+    print 'Finding resistomes for file: {0}'.format(f.split('/')[-1])
+    fastq_read_1 = f + '_1_sequence_kneaddata_paired_1.fastq'
+    fastq_read_2 = f + '_1_sequence_kneaddata_paired_2.fastq'
+    fastq_unpaired_1 = f + '_1_sequence_kneaddata_unmatched_1.fastq'
+    fastq_unpaired_2 = f + '_1_sequence_kneaddata_unmatched_2.fastq'
+    
+    out_file = out_path + f.split('/')[-1] + '_shortbred.txt'
+    
+    command = '{0} --markers {1} --wgs {2} {3} {4} {5} --results {6} --id {7}  --threads 20 --usearch ../tools/usearch-v10.0.240 --tmp ./temp_files/quantification_{8}'.format(shortbred, markers, fastq_read_1, fastq_read_2, fastq_unpaired_1, fastq_unpaired_2, out_file, id, random.randint(1,1000))
+    
+    final = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+    (stdout, stderr) = final.communicate()
+    print stdout
+    print stderr
+
+def main():
+    # Get arguments
+    parser = argparse.ArgumentParser(description='Specify arguments for ShortBRED quantification identification of ARGs.')
+    parser.add_argument('--fastq_folder',
+                        help='path to folder containing quality controlled fastq files')
+    parser.add_argument('--ref_markers',
+                        help='path to ARG markers')
+    parser.add_argument('--out',
+                        help='folder for all ShortBRED output files')
+    parser.add_argument('--shortbred',
+                        help='path to shortbred software')
+    parser.add_argument('--id', default= 0.99,
+                        help='minimum identity for alignment')
+    parser.add_argument('--threads', default= 1.0,
+                        help='number file sets to analyze')
+    args = parser.parse_args()
+
+    global shortbred, out_path, markers, shrotbred, id
+
+    #Get all fastq file in folder (NOTE: fastq files should be QC'ed)
+    fastq_folder = args.fastq_folder
+    files = set([f.split('_1_')[0] for f in glob.glob(fastq_folder + '*.fastq*')])
+    out_path = args.out
+
+    #shortbred parameters
+    markers = args.ref_markers
+    shortbred = args.shortbred
+    id = args.id
+
+    #multiprocessing
+    threads = args.threads
+    p = multiprocessing.Pool(int(threads))
+    p.map(quantify_resistomes, files)
+    p.close()
+    p.join()
+
+main()
